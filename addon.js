dd748a1c8577e2a020a0c6b2aff07b5e712abc03
PinkDev1 <5990@protonmail.com>
2020-02-13 01:03:43 -0300

    Improved CSS & Improved liveComments hiding
    Changed some magic numbers that were in the CSS styling section and switched them up with their equitative percentages.
    
    Improved the way liveComments are hiden: Instead of deleting the HTML element, a CSS propierty is applied to it to make it go away, so that the liveComments HTML element isn't reborn after you scroll down to another blog.

--- addon.js
+++ addon.js
@@ -6,41 +6,42 @@ async function main () {
 
     //Find the annoying html elements and store them in their respective arrays
     var banner = await document.getElementsByClassName("content community-bg");
-    console.log(banner);
     var navButtons = await document.getElementsByClassName("home-fixed-container");
-    var liveComments = await document.getElementsByClassName("live-comment");
-    console.log(liveComments);
     var communityListBlur = await document.getElementsByClassName("global-chat-community-bg");
-    console.log(communityListBlur); 
 
     await console.info("[AHS]: Modifying page...");
 
-    try {
-        //Delete unwanted elements...
-        banner[0].parentNode.removeChild(banner[0]);
-        communityListBlur[0].parentNode.removeChild(communityListBlur[0]);
+    //if the banner was collected succesfully...
+    if(banner[0].lenght !== 0){
+        try {
+            //Delete unwanted elements...
+            banner[0].parentNode.removeChild(banner[0]);
+            communityListBlur[0].parentNode.removeChild(communityListBlur[0]);
 
-        //Customize the looks of the NavButtons
-        navButtons[0].style.marginTop = "-32px";
-        navButtons[0].style.width = "100%";
-        navButtons[0].style.maxWidth = "100%";
-        navButtons[0].style.background = "rgba(0,0,0,0)";
-        navButtons[0].style.paddingLeft = "0px";
-        navButtons[0].style.paddingRight = "0px";
-        navButtons[0].style.marginLeft = "-200px";
-        navButtons[0].style.paddingTop = "0px";
-        console.log(navButtons);
+            //Customize the looks of the NavButtons
+            navButtons[0].style.marginTop = "-2.5%";
+            navButtons[0].style.width = "auto";
+            navButtons[0].style.maxWidth = "100%";
+            navButtons[0].style.background = "rgba(0,0,0,0)";
+            navButtons[0].style.paddingLeft = "0px";
+            navButtons[0].style.paddingRight = "0px";
+            navButtons[0].style.marginLeft = "-14.275%";
+            navButtons[0].style.paddingTop = "0px";
 
-    } catch(e) {
-        
-        console.error("[AHS]: ERROR: ");
-        console.log(e);
+        } catch(e) {
+            
+            console.error("[AHS]: ERROR: ");
+            console.log(e);
+        }
     }
+    
     try {
-        //Live comments are deleted separetly because they are not always on the page...
-        liveComments[0].parentNode.removeChild(liveComments[0]);
+        //Live comments are edited separetly because they are not always on the page...
+        var liveComments = await document.getElementsByClassName("live-comment");
+        var liveCommentsCSS = await liveComments[0].getElementsByClassName("content init");
+        liveCommentsCSS[0].style.position = "initial";
     } catch(e) {
-    	
+
         console.error("[AHS]: ERROR: ");
         console.log(e);
     }
