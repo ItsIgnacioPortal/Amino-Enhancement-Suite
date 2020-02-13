8d780ec38698ab0e9f0e5fa7998ab04883963dc6
PinkDev1 <5990@protonmail.com>
2020-02-13 18:11:46 -0300

    Multiple fixes & Improvements
    Compacted grabbing of "liveComments" to one var
    Implemented customization of the liveMembers section
    Implemented deletion of the Overflow button in the NavButtons

--- addon.js
+++ addon.js
@@ -2,49 +2,59 @@
 console.info("AHS Loaded.");
 main();
 
-async function main () {
+async function main(){
 
-    //Find the annoying html elements and store them in their respective arrays
-    var banner = await document.getElementsByClassName("content community-bg");
-    var navButtons = await document.getElementsByClassName("home-fixed-container");
-    var communityListBlur = await document.getElementsByClassName("global-chat-community-bg");
+	//Find the annoying html elements and store them in their respective arrays
+	var banner = await document.getElementsByClassName("content community-bg");
+	var navButtons = await document.getElementsByClassName("home-fixed-container");
+	var navButtonsOverflow = await document.getElementsByClassName("overflow-hint");
+	var communityListBlur = await document.getElementsByClassName("global-chat-community-bg");
+	var liveComments = await document.getElementsByClassName("live-comment")[0].getElementsByClassName("content init");
+	var liveMembers = await document.getElementsByClassName("live-member-list active");
 
-    await console.info("[AHS]: Modifying page...");
+	await console.info("[AHS]: Modifying page...");
 
-    //if the banner was collected succesfully...
-    if(banner[0].lenght !== 0){
-        try {
-            //Delete unwanted elements...
-            banner[0].parentNode.removeChild(banner[0]);
-            communityListBlur[0].parentNode.removeChild(communityListBlur[0]);
+	//if the banner was collected succesfully...
+	if(banner[0].lenght !== 0){
+		try {
+			//Delete unwanted elements...
+			banner[0].parentNode.removeChild(banner[0]);
+			communityListBlur[0].parentNode.removeChild(communityListBlur[0]);
+			navButtonsOverflow[0].parentNode.removeChild(navButtonsOverflow[0]);
 
-            //Customize the looks of the NavButtons
-            navButtons[0].style.marginTop = "-2.5%";
-            navButtons[0].style.width = "auto";
-            navButtons[0].style.maxWidth = "100%";
-            navButtons[0].style.background = "rgba(0,0,0,0)";
-            navButtons[0].style.paddingLeft = "0px";
-            navButtons[0].style.paddingRight = "0px";
-            navButtons[0].style.marginLeft = "-14.275%";
-            navButtons[0].style.paddingTop = "0px";
+			//Customize the NavButtons
+			navButtons[0].style.marginTop = "-2.5%";
+			navButtons[0].style.width = "auto";
+			navButtons[0].style.maxWidth = "100%";
+			navButtons[0].style.background = "rgba(0,0,0,0)";
+			navButtons[0].style.paddingLeft = "0px";
+			navButtons[0].style.paddingRight = "0px";
+			navButtons[0].style.marginLeft = "-14.275%";
+			navButtons[0].style.paddingTop = "0px";
 
-        } catch(e) {
-            
-            console.error("[AHS]: ERROR: ");
-            console.log(e);
-        }
-    }
-    
-    try {
-        //Live comments are edited separetly because they are not always on the page...
-        var liveComments = await document.getElementsByClassName("live-comment");
-        var liveCommentsCSS = await liveComments[0].getElementsByClassName("content init");
-        liveCommentsCSS[0].style.position = "initial";
-    } catch(e) {
+		} catch(e) {
+			
+			console.error("[AHS]: ERROR: ");
+			console.log(e);
+		}
+	}
+	
+	//Live comments are edited separetly because they are not always on the page...
 
-        console.error("[AHS]: ERROR: ");
-        console.log(e);
-    }
-    
+	//When the page loads...
+	window.onload = async function() {
+		try {
+			//Hide the liveComments
+			liveComments[0].style.position = "initial";
 
+			//Customize the liveMembers position
+			liveMembers[0].style.position = "fixed";
+			liveMembers[0].style.marginLeft = "0.5%";
+
+		} catch(e) {
+			console.error("[AHS]: ERROR: ");
+			console.log(e);
+
+		}
+	};
 }
