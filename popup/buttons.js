a57a8787d266cbaee8a3fc35dfe6d6cf42e83bc7
PinkDev1 <5990@protonmail.com>
2020-02-28 01:44:36 -0300

    Multiple changes
    buttons.js: Added comments
    
    style.css & menu.html: small fixes and removed the green button as it was not necessary

--- popup/buttons.js
+++ popup/buttons.js
@@ -1,10 +1,12 @@
 window.onload = async function(){
 
+	//Get the wrapper of the github button
 	var githubButton = await document.getElementById("githubButtonWrapper");
+
+	//Add an event listener onClick
 	await githubButton.addEventListener("click", (function(tab) {
 		browser.tabs.create({
 			url: "https://github.com/PinkDev1/Amino-Enhancement-Suite"
 		});
 	}));
-	console.log("hellowo");
 }
